ac71021d48623beed766c0f6539c14826bec08b2
곽재영 <kjyjykwak@naver.com>
2021-12-15 04:18:07 +0900

    Create 10998.cpp

---
+++ src/10998.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/10998
+// written by julyy
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+	cin >> a >> b;
+	cout << a * b << endl;
+    
+    return 0;
+}
