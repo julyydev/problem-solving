29e82b07a185ced29f3246d596203477e52e08f8
Julyy <66120479+kwakjy@users.noreply.github.com>
2021-12-15 03:50:20 +0900

    Create 10718.cpp

---
+++ src/10718.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/2557
+// written by julyy
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군" << endl;
+    return 0;
+}
