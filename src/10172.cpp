e6ea6188379dcc85300548d0ff318090c17b75b5
곽재영 <kjyjykwak@naver.com>
2021-12-15 04:14:04 +0900

    Create 10172.cpp

---
+++ src/10172.cpp
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/10172
+// written by julyy
+
+/*
+"나 ' 출력하려면 \ 붙여서 사용
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+
+    return 0;
+}
