9a0637077cc5825d5c44c4af38f799dc943db8de
곽재영 <kjyjykwak@naver.com>
2021-12-15 04:36:21 +0900

    Create 2742.cpp

---
+++ src/2742.cpp
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/2742
+// written by julyy
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    for (int i = n; i > 0; i--) {
+        cout << i << "\n";
+    }
+
+	return 0;
+}
