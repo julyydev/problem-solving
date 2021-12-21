f45c62a2ae5c33bdf8544c3fdbf87f545650ffc7
곽재영 <kjyjykwak@naver.com>
2021-12-21 22:59:18 +0900

    Create 2420.cpp

---
+++ src/2420.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/2420
+// written by julyy
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    cout << abs(n - m);
+    
+    return 0;
+}
