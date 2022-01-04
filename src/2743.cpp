5261515185579054f0e1f00a60e1d5e1bc559d37
곽재영 <kjyjykwak@naver.com>
2022-01-04 10:37:12 +0900

    Create 2743.cpp

---
+++ src/2743.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/2743
+// written by julyy
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s.length();
+    
+    return 0;
+}
