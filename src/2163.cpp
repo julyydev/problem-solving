b0a12de0282e70a9927cc3a68c6a0355cc46ebba
곽재영 <kjyjykwak@naver.com>
2022-01-19 22:03:35 +0900

    Create 2163.cpp

---
+++ src/2163.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/2163
+// written by julyy
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << n * m - 1;
+    
+    return 0;
+}
