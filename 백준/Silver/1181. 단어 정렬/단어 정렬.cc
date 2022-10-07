a772b6e34664b261e0a3223eee00f5ce09b8637e
Jaeyoung Kwak <kjyjykwak@naver.com>
2022-10-07 20:47:35 +0900

    [Silver V] Title: 단어 정렬, Time: 44 ms, Memory: 4648 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,45 @@
+// https://www.acmicpc.net/problem/1181
+// written by julyy
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool compare(string a, string b) {
+    if (a.length() == b.length()) {
+        int i = 0;
+        while (a[i] == b[i]) {
+            if (a[i] == '\0' || b[i] == '\0')
+                return false;
+            i++;
+        }
+        return a[i] < b[i];
+    }
+    return a.length() < b.length();
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<string> vec;
+    int N;
+    string input;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> input;
+        vec.push_back(input);
+    }
+
+    sort(vec.begin(), vec.end(), compare);
+    vec.erase(unique(vec.begin(), vec.end()), vec.end());
+
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << '\n';
+    }
+    
+    return 0;
+}
