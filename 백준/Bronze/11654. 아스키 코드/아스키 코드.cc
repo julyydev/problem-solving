7170fcc31e344fd532280ca6c2dfab489c761d36
Jaeyoung Kwak <kjyjykwak@naver.com>
2022-10-07 21:20:44 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/11654
+// written by julyy
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char input;
+    cin >> input;
+    cout << (int)input << '\n';
+
+    return 0;
+}
