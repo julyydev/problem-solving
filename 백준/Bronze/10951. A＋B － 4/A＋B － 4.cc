dc0cb558e8c97d67af80e687f615f7cc4a422c8d
Jaeyoung Kwak <kjyjykwak@naver.com>
2022-10-07 21:01:45 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,18 @@
+// https://www.acmicpc.net/problem/10951
+// written by julyy
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int a, b;
+    
+    while(cin >> a >> b)
+        cout << a + b << endl;
+
+	return 0;
+}
