7ea24408f4e4c5700a900529d2da97d8144476af
Jaeyoung Kwak <kjyjykwak@naver.com>
2022-10-07 20:53:01 +0900

    [Silver II] Title: 조합 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
@@ -0,0 +1,37 @@
+// https://www.acmicpc.net/problem/2004
+// written by julyy
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    long long n, m, num_of_two = 0, num_of_five = 0;
+    cin >> n >> m;
+
+    if (!m) {
+        cout << 0;
+        return 0;
+    }
+    if (2 * m > n)
+        m = n - m;
+    
+    for (long long i = 2; i <= n; i *= 2)
+        num_of_two += n / i;
+    for (long long i = 5; i <= n; i *= 5)
+        num_of_five += n / i;
+
+    for (long long i = 2; i <= m; i *= 2)
+        num_of_two -= m / i;
+    for (long long i = 5; i <= n; i *= 5)
+        num_of_five -= m / i;
+
+    for (long long i = 2; i <= n - m; i *= 2)
+        num_of_two -= (n - m) / i;
+    for (long long i = 5; i <= n - m; i *= 5)
+        num_of_five -= (n - m) / i;
+
+    cout << min(num_of_two, num_of_five);
+    
+    return 0;
+}
