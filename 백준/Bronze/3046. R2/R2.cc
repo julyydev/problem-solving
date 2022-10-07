75acc96e40befebbf0302586bddd116c008095d2
Jaeyoung Kwak <kjyjykwak@naver.com>
2022-10-07 21:00:32 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/3046
+// written by julyy
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, s;
+    cin >> r >> s;
+    cout << 2 * s - r;
+    
+    return 0;
+}
