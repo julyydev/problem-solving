da196f8b883b6b83c761d4637469e191e19695e3
Jaeyoung Kwak <kjyjykwak@naver.com>
2022-10-07 21:02:10 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/11718
+// written by julyy
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    while(getline(cin, s)) {
+        cout << s << '\n';
+    }
+    
+    return 0;
+}
