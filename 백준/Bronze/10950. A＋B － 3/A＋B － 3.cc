478c8c9bb61364947e42d0b0d90db735fb3cd023
Jaeyoung Kwak <kjyjykwak@naver.com>
2022-10-07 21:19:04 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/10950
+// written by julyy
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
+	return 0;
+}
