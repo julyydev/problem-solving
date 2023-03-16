959fc9d0e0f6dc2441513aadd3ecdcaba7b75501
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-03-17 06:00:51 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> s;
+        cout << s[0] << s[s.size() - 1] << "\n";
+    }
+
+    return 0;
+}
