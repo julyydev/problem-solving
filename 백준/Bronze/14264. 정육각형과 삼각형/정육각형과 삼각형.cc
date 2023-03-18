ea85665f6a6ea6d2c4d708c1569050b7d699d1b7
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-03-18 23:28:43 +0900

    [Bronze III] Title: 정육각형과 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.cc
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long l;
+    cin >> l;
+    cout.precision(20);
+    cout << l * l * sqrt(3) / 4 << "\n";
+
+    return 0;
+}
