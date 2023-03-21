9322142975a533ad30ff496bee901f83d55f4c6e
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-03-22 02:19:06 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.cc
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -9,30 +9,11 @@ int main() {
     cin >> n;
     dp.resize(n + 1, 0);
 
-    dp[0] = 0;
-    for (int i = 1; i * i <= n; i++) dp[i * i] = 1;
+    dp[1] = 1;
     for (int i = 1; i <= n; i++) {
-        if (dp[i] == 0) {
-            for (int j = 1; j <= i / 2; j++) {
-                if (dp[j] == 1 && dp[i - j] == 1) {
-                    dp[i] = 2;
-                    break;
-                }
-            }
-        }
+        dp[i] = dp[1] + dp[i - 1];
+        for (int j = 2; j * j <= i; j++) dp[i] = min(dp[i], 1 + dp[i - j * j]);
     }
-    for (int i = 1; i <= n; i++) {
-        if (dp[i] == 0) {
-            for (int j = 1; j <= i; j++) {
-                if (dp[j] == 1 && dp[i - j] == 2) {
-                    dp[i] = 3;
-                    break;
-                }
-            }
-        }
-    }
-    for (int i = 1; i <= n; i++)
-        if (dp[i] == 0) dp[i] = 4;
 
     cout << dp[n] << "\n";
 
