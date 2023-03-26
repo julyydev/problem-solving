ba406c59af3798ef2ac4e5ed57175f2053d9895c
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-03-26 19:51:02 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -1,40 +1,47 @@
-#include <algorithm>
 #include <iostream>
 #include <queue>
 #include <vector>
 #define FASTIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 using namespace std;
 
-struct Point {
-    int x, y;
-    Point(int x, int y) : x(x), y(y) {}
-    bool operator<(const Point& p) const {
-        if (x == p.x) return y < p.y;
-        return x < p.x;
+struct Position {
+    int x, y, dist;
+    Position(int x, int y, int dist) : x(x), y(y), dist(dist) {}
+    bool operator<(const Position& d) const {
+        if (dist == d.dist) {
+            if (x == d.x) return y > d.y;
+            return x > d.x;
+        }
+        return dist > d.dist;
     }
 };
 
-struct Data {
-    Point point;
-    int time;
-    Data(Point point, int time) : point(point), time(time) {}
-};
-
-class Shark {
-    Point point;
-    int size;
-    int eaten_num;
-    void grow() { size++, eaten_num = 0; }
+template <typename T>
+class Vector2D {
+    vector<vector<T>> vector_2d;
 
    public:
-    Shark() : point({0, 0}), size(2), eaten_num(0) {}
-    Shark(Point point) : point(point), size(2), eaten_num(0) {}
-    Point getPoint() { return point; }
-    void setPoint(Point next) { point = Point(next); }
-    int getSize() { return size; }
+    Vector2D() {}
+    void init(int n, int m, int init_value) {
+        vector_2d.assign(n, vector<T>(m, init_value));
+    }
+    T getValue(int x, int y) { return vector_2d[x][y]; }
+    T getValue(Position p) { return vector_2d[p.x][p.y]; }
+    void setValue(int x, int y, T value) { vector_2d[x][y] = value; }
+    void setValue(Position p, T value) { vector_2d[p.x][p.y] = value; }
+};
+
+struct Shark {
+    Position position;
+    int size;
+    int eaten_num;
+
+    Shark() : position({0, 0, 0}), size(2), eaten_num(0) {}
     void eat() {
         eaten_num++;
-        if (eaten_num == size) grow();
+        if (eaten_num == size) {
+            size++, eaten_num = 0;
+        }
     }
 };
 
@@ -42,7 +49,7 @@ class Fishes {
     vector<int> fish_nums;
 
    public:
-    Fishes() { fish_nums.resize(7, 0); }
+    Fishes() { fish_nums.assign(7, 0); }
     void executePrefixSum() {
         for (int i = 2; i <= 6; i++) fish_nums[i] += fish_nums[i - 1];
     }
@@ -57,56 +64,51 @@ class Fishes {
 };
 
 int N;
-vector<vector<int>> map;
-vector<vector<bool>> isVisited;
 int dx[4] = {-1, 0, 0, 1};
 int dy[4] = {0, -1, 1, 0};
 
-int bfs(Shark& shark, Fishes& fishes) {
-    isVisited.clear();
-    isVisited.resize(N, vector<bool>(N, false));
-    vector<Point> possiblePoints;
-    int min_time = INT32_MAX;
-    queue<Data> q;
-    q.push({shark.getPoint(), 0});
-    isVisited[shark.getPoint().x][shark.getPoint().y] = true;
+int bfs(Shark& shark, Fishes& fishes, Vector2D<int>& map,
+        Vector2D<bool>& isVisited) {
+    isVisited.init(N, N, false);
+    priority_queue<Position> pq;
+    pq.push({shark.position});
+    isVisited.setValue(shark.position, true);
 
-    while (!q.empty()) {
-        Data cur = q.front();
-        q.pop();
-        if (cur.time >= min_time) break;
+    while (!pq.empty()) {
+        Position cur = pq.top();
+        pq.pop();
+        int fish_size = map.getValue(cur);
+        if (fish_size > 0 && fish_size < shark.size) {
+            shark.eat();
+            shark.position = {cur.x, cur.y, 0};
+            map.setValue(cur, 0);
+            fishes.decreaseFishNum(fish_size);
+            return cur.dist;
+        }
 
         for (int i = 0; i < 4; i++) {
-            Point next(cur.point.x + dx[i], cur.point.y + dy[i]);
-            if (next.x < 0 || next.x >= N || next.y < 0 || next.y >= N)
+            Position next(cur.x + dx[i], cur.y + dy[i], cur.dist + 1);
+            if (next.x < 0 || next.x >= N || next.y < 0 || next.y >= N ||
+                isVisited.getValue(next))
                 continue;
-            if (isVisited[next.x][next.y]) continue;
-
-            int fish_size = map[next.x][next.y];
-            if (fish_size > 0 && fish_size < shark.getSize()) {
-                possiblePoints.push_back(next);
-                if (min_time == INT32_MAX) min_time = cur.time + 1;
-            } else if (fish_size == 0 || fish_size == shark.getSize()) {
-                q.push({next, cur.time + 1});
-                isVisited[next.x][next.y] = true;
+            if (map.getValue(next) > shark.size)
+                continue;
+            else {
+                pq.push({next});
+                isVisited.setValue(next, true);
             }
         }
     }
 
-    if (possiblePoints.size() == 0) return min_time;
-
-    sort(possiblePoints.begin(), possiblePoints.end());
-    shark.eat();
-    shark.setPoint(possiblePoints[0]);
-    map[possiblePoints[0].x][possiblePoints[0].y] = 0;
-    fishes.decreaseFishNum(map[possiblePoints[0].x][possiblePoints[0].y]);
-    return min_time;
+    return 0;
 }
 
 int main() {
     FASTIO();
     cin >> N;
-    map.resize(N, vector<int>(N));
+    Vector2D<int> map;
+    Vector2D<bool> isVisited;
+    map.init(N, N, 0);
     Shark shark;
     Fishes fishes;
 
@@ -114,10 +116,10 @@ int main() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cin >> x;
-            map[i][j] = x;
+            map.setValue(i, j, x);
             if (x == 9) {
-                map[i][j] = 0;
-                shark = Shark({i, j});
+                map.setValue(i, j, 0);
+                shark.position = {i, j, 0};
             } else if (x >= 1 || x <= 6)
                 fishes.increaseFishNum(x);
         }
@@ -125,9 +127,9 @@ int main() {
     fishes.executePrefixSum();
 
     int total_time = 0, time;
-    while (fishes.getFishNum(shark.getSize())) {
-        time = bfs(shark, fishes);
-        if (time == INT32_MAX) break;
+    while (fishes.getFishNum(shark.size)) {
+        time = bfs(shark, fishes, map, isVisited);
+        if (time == 0) break;
         total_time += time;
     }
     cout << total_time << "\n";
