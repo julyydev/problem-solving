36d262239e71e9a67656db2894d81bd2f1be6baa
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-03-31 23:18:11 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    stack<char> stack;
+
+    for (char c : s) {
+        if (c >= 'A' && c <= 'Z') {
+            cout << c;
+            continue;
+        }
+
+        if (c == '+' || c == '-') {
+            if (stack.empty()) {
+                stack.push(c);
+                continue;
+            }
+
+            while (!stack.empty() && stack.top() != '(') {
+                cout << stack.top();
+                stack.pop();
+            }
+            stack.push(c);
+        } else if (c == '*' || c == '/') {
+            if (stack.empty()) {
+                stack.push(c);
+                continue;
+            }
+
+            while (!stack.empty() && stack.top() != '(' &&
+                   (stack.top() == '*' || stack.top() == '/')) {
+                cout << stack.top();
+                stack.pop();
+            }
+            stack.push(c);
+        } else if (c == '(')
+            stack.push(c);
+        else if (c == ')') {
+            while (stack.top() != '(') {
+                cout << stack.top();
+                stack.pop();
+            }
+            stack.pop();
+        }
+    }
+    while (!stack.empty()) {
+        cout << stack.top();
+        stack.pop();
+    }
+    cout << "\n";
+
+    return 0;
+}
