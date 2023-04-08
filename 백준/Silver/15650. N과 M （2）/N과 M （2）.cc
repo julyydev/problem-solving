a030c3218a5d22f9e689f50a6f62737aec4a7e2b
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-04-08 19:49:39 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void backtracking(const int& n, const int& m, vector<int>& vec) {
+    if (vec.size() == m) {
+        for (int v : vec) cout << v << " ";
+        cout << "\n";
+        vec.pop_back();
+        return;
+    }
+
+    for (int i = (vec.empty() ? 1 : vec.back() + 1); i <= n; i++) {
+        vec.push_back(i);
+        backtracking(n, m, vec);
+    }
+    if (!vec.empty()) vec.pop_back();
+    return;
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> vec;
+    backtracking(N, M, vec);
+
+    return 0;
+}
