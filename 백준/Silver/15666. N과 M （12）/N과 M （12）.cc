a3b1f97e70f1238a1f37a0592aac22cf944fea68
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-04-08 21:39:07 +0900

    [Silver II] Title: N과 M (12), Time: 4 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+void backtracking(const int& n, const int& m, const vector<int>& arr,
+                  vector<int>& vec, set<vector<int>>& set) {
+    if (vec.size() == m) {
+        vector<int> newVec(m);
+        for (int i = 0; i < m; i++) newVec[i] = arr[vec[i]];
+        set.insert(newVec);
+        vec.pop_back();
+        return;
+    }
+
+    for (int i = (vec.empty() ? 0 : vec.back()); i < n; i++) {
+        vec.push_back(i);
+        backtracking(n, m, arr, vec, set);
+    }
+    if (!vec.empty()) vec.pop_back();
+    return;
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> arr(N), vec;
+    set<vector<int>> set;
+    for (int i = 0; i < N; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+
+    backtracking(N, M, arr, vec, set);
+    for (auto s : set) {
+        for (int v : s) cout << v << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
