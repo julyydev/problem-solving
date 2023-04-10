618bd475ac1c4ac0f00606363fb6b1ef70c3faa8
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-04-10 19:17:00 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#define FASTIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+
+int main() {
+    FASTIO();
+    int N, K, A, B;
+    cin >> N >> K;
+    while (N--) cin >> A >> B;
+    cout << "비와이\n";
+
+    return 0;
+}
