ec28562fd34cf178118c9e3c5506e452c8270075
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-04-15 18:33:50 +0900

    [Gold II] Title: 트리의 지름, Time: 76 ms, Memory: 8564 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#define FASTIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+
+struct Node {
+    int e, w;
+    Node(int e, int w) : e(e), w(w) {}
+};
+
+void dfs(int cur, int& max_length, int& length, int& max_point,
+         const vector<vector<Node>>& graph, vector<bool>& isVisited) {
+    isVisited[cur] = true;
+    for (int i = 0; i < graph[cur].size(); i++) {
+        int next = graph[cur][i].e;
+        if (isVisited[next]) continue;
+
+        int temp = length + graph[cur][i].w;
+        if (temp > max_length) {
+            max_length = temp;
+            max_point = next;
+        }
+        dfs(next, max_length, temp, max_point, graph, isVisited);
+    }
+    return;
+}
+
+int main() {
+    FASTIO();
+    int V, s, e, w;
+    cin >> V;
+    vector<vector<Node>> graph(V + 1);
+    vector<bool> isVisited(V + 1, false);
+    for (int i = 0; i < V; i++) {
+        cin >> s;
+        while (1) {
+            cin >> e;
+            if (e == -1) break;
+            cin >> w;
+            graph[s].push_back({e, w});
+        }
+    }
+
+    int max_length = 0, length = 0, max_point = 0;
+    dfs(1, max_length, length, max_point, graph, isVisited);
+    isVisited.assign(V + 1, false);
+    max_length = 0;
+    dfs(max_point, max_length, length, max_point, graph, isVisited);
+    cout << max_length << "\n";
+
+    return 0;
+}
