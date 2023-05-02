63dbdc7fdcb39acbabe41eb6dba0d7e1a2a6bbf8
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-05-02 17:00:00 +0900

    [Gold I] Title: 트리의 순회, Time: 36 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.cc
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.cc
@@ -4,20 +4,16 @@
 #define FASTIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 using namespace std;
 
-vector<int> inorder, postorder;
+vector<int> inorder, postorder, indexs;
 
-void recursion(const int& inorder_index, const int& postorder_index,
-               const int& size) {
-    cout << postorder[postorder_index + size - 1] << " ";
-    int in_index = find(inorder.begin(), inorder.end(),
-                        postorder[postorder_index + size - 1]) -
-                   inorder.begin();
-    int left_size = in_index - inorder_index;
+void recursion(const int& in_index, const int& post_index, const int& size) {
+    cout << postorder[post_index + size - 1] << " ";
+    int target = indexs[postorder[post_index + size - 1]];
+    int left_size = target - in_index;
     int right_size = size - left_size - 1;
 
-    if (left_size) recursion(inorder_index, postorder_index, left_size);
-    if (right_size)
-        recursion(in_index + 1, postorder_index + left_size, right_size);
+    if (left_size) recursion(in_index, post_index, left_size);
+    if (right_size) recursion(target + 1, post_index + left_size, right_size);
 
     return;
 }
@@ -26,9 +22,12 @@ int main() {
     FASTIO();
     int n;
     cin >> n;
-    inorder.resize(n), postorder.resize(n);
+    inorder.resize(n), postorder.resize(n), indexs.resize(n + 1);
 
-    for (int i = 0; i < n; i++) cin >> inorder[i];
+    for (int i = 0; i < n; i++) {
+        cin >> inorder[i];
+        indexs[inorder[i]] = i;
+    }
     for (int i = 0; i < n; i++) cin >> postorder[i];
     recursion(0, 0, postorder.size());
     cout << "\n";
