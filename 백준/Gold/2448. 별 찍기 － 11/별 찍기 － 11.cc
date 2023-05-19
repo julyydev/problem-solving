c89f191640785c86a8ddf65163aa280304cf3b4b
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-05-19 19:45:41 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 436 ms, Memory: 4508 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#define FASTIO() ios_base::sync_with_stdio(false), cin.tie(NULL)
 using namespace std;
 
 void recursion(int x, int y, int n, vector<vector<bool>>& star) {
-    if (n == 1) {
+    if (n == 3) {
         star[x][y] = star[x + 1][y - 1] = star[x + 1][y + 1] =
             star[x + 2][y - 2] = star[x + 2][y - 1] = star[x + 2][y] =
                 star[x + 2][y + 1] = star[x + 2][y + 2] = true;
@@ -11,15 +12,16 @@ void recursion(int x, int y, int n, vector<vector<bool>>& star) {
     }
 
     recursion(x, y, n / 2, star);
-    recursion(x + 3 * n / 2, y - 3 * n / 2, n / 2, star);
-    recursion(x + 3 * n / 2, y + 3 * n / 2, n / 2, star);
+    recursion(x + n / 2, y - n / 2, n / 2, star);
+    recursion(x + n / 2, y + n / 2, n / 2, star);
 }
 
 int main() {
+    FASTIO();
     int N;
     cin >> N;
     vector<vector<bool>> star(N, vector<bool>(2 * N - 1, false));
-    recursion(0, N - 1, N / 3, star);
+    recursion(0, N - 1, N, star);
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < 2 * N - 1; j++) cout << (star[i][j] ? "*" : " ");
