e16f4e03a6d7710c92e2b1064dcd7fc3b58d646e
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-05-22 22:53:22 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x, sum = 0;
+    cin >> n;
+    
+    while (n--) {
+        cin >> x;
+        sum += x;
+    }
+    cout << sum << "\n";
+    return 0;
+}
