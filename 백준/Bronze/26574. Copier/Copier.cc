3a7b3fe4fbd532cd584e93652519c618ec51aba6
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-05-26 23:30:38 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n;
+    
+    while (n--) {
+        cin >> x;
+        cout << x << " " << x << "\n";
+    }
+    
+    return 0;
+}
