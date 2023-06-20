087e8710e16e557dabfc1d915d03c9d59dd9e799
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-06-20 15:49:39 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 12 ms, Memory: 5152 KB -BaekjoonHub

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#define FASTIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+
+int get_distance(string s1, string s2) {
+    int result = 0;
+    for (int i = 0; i < 4; i++)
+        if (s1[i] != s2[i]) result++;
+    return result;
+}
+
+int main() {
+    FASTIO();
+    int T, N;
+    cin >> T;
+
+    while (T--) {
+        cin >> N;
+        vector<string> mbtis(N);
+        string s;
+        for (int i = 0; i < N; i++) {
+            cin >> s;
+            mbtis[i] = s;
+        }
+
+        if (N >= 33) {
+            cout << "0\n";
+            continue;
+        }
+
+        int min_distance = 100;
+        for (int i = 0; i < mbtis.size() - 2; i++) {
+            for (int j = i + 1; j < mbtis.size() - 1; j++) {
+                for (int k = j + 1; k < mbtis.size(); k++) {
+                    int distance = get_distance(mbtis[i], mbtis[j]) +
+                                   get_distance(mbtis[i], mbtis[k]) +
+                                   get_distance(mbtis[j], mbtis[k]);
+                    if (distance < min_distance) min_distance = distance;
+                }
+            }
+        }
+        cout << min_distance << "\n";
+    }
+
+    return 0;
+}
