9d208806ea87a2088fdba93656c98e39a995188c
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-06-20 15:51:25 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 12 ms, Memory: 5152 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
@@ -30,9 +30,9 @@ int main() {
         }
 
         int min_distance = 100;
-        for (int i = 0; i < mbtis.size() - 2; i++) {
-            for (int j = i + 1; j < mbtis.size() - 1; j++) {
-                for (int k = j + 1; k < mbtis.size(); k++) {
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
                     int distance = get_distance(mbtis[i], mbtis[j]) +
                                    get_distance(mbtis[i], mbtis[k]) +
                                    get_distance(mbtis[j], mbtis[k]);
