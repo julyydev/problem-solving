a30e6e0552c6d15ad40ccf10b69fce8e02767702
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-07-29 22:22:16 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0, x;
+    
+    for (int i = 0; i < 5; i++) {
+        cin >> x;
+        sum += x;
+    }
+    
+    cout << sum << "\n";
+    
+    return 0;
+}
