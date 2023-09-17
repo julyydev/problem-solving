3dce58068d607efe942964e119633fcae8635ed9
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-09-17 22:14:13 +0900

    [Bronze V] Title: Serca, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#define FASTIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+
+int main() {
+    FASTIO();
+    int n;
+    cin >> n;
+
+    while (n--) {
+        cout << " @@@   @@@ \n";
+        cout << "@   @ @   @\n";
+        cout << "@    @    @\n";
+        cout << "@         @\n";
+        cout << " @       @ \n";
+        cout << "  @     @  \n";
+        cout << "   @   @   \n";
+        cout << "    @ @    \n";
+        cout << "     @     \n";
+    }
+
+    return 0;
+}
