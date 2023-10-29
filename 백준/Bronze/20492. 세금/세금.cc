7a329e7532ddd93eaa8fe3bf7ab2f62fa8e37985
Jaeyoung Kwak <kjyjykwak@naver.com>
2023-10-29 20:16:30 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    
+    cout << n * 78 / 100 << "\n";
+    cout << n * 956 / 1000 << "\n";
+    
+    return 0;
+}
