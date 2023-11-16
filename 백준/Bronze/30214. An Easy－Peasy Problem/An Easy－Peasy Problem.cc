d27ae528d11a7f9175d7a4eeac7228662284577f
Jaeyoung Kwak <kjyjykwak@gmail.com>
2023-11-16 23:18:28 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    if (a * 2 >= b) cout << "E\n";
+    else cout << "H\n";
+    
+    return 0;
+}
