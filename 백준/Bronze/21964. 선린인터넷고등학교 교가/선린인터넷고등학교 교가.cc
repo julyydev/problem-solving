a473511ceba2f2ef779f07c0bb32e9b4ceae27a6
Jaeyoung Kwak <kjyjykwak@gmail.com>
2023-12-17 22:53:40 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    
+    for (int i = n - 5; i < n; i++)
+        cout << s[i];
+    cout << "\n";
+    
+    return 0;
+}
