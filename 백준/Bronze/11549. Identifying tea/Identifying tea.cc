4b3ad6c1248d4b310d5254ca7f8997fdc70c5af1
Jaeyoung Kwak <kjyjykwak@gmail.com>
2024-01-15 21:52:42 +0900

    [Bronze IV] Title: Identifying tea, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x, count = 0;
+    cin >> n;
+    
+    for (int i = 0; i < 5; i++) {
+        cin >> x;
+        if (x == n) count++;
+    }
+    cout << count << "\n";
+    
+    return 0;
+}
