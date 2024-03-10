ac4f137b505cd0b3652708ec24c2003ab07c212b
Jaeyoung Kwak <kjyjykwak@gmail.com>
2024-03-10 23:54:52 +0900

    [Bronze IV] Title: 특식 배부, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a, b, c;
+    cin >> n >> a >> b >> c;
+    cout << min(n, a) + min(n, b) + min(n, c) << "\n";
+    
+    return 0;
+}
