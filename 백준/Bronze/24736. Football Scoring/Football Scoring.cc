1abea1f05e3cf6fee3e31286a579adbaf64d7db2
Jaeyoung Kwak <kjyjykwak@gmail.com>
2024-04-13 23:30:31 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+    for (int i = 0; i < 2; i++) {
+        cin >> a >> b >> c >> d >> e;
+        cout << 6 * a + 3 * b + 2 * c + d + 2 * e << " ";
+    }
+    cout << "\n";
+    
+    return 0;
+}
