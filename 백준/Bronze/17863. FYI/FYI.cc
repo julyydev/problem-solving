cd8739cd1966a7ecb38220c9a8e1c210f3c39d51
Jaeyoung Kwak <kjyjykwak@gmail.com>
2024-05-06 22:48:09 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    
+    if (s[0] == '5' && s[1] == '5' && s[2] == '5') cout << "YES\n";
+    else cout << "NO\n";
+    
+    return 0;
+}
