e6a1acce736b6d361fc6a9c59068ba3069109fc7
Jaeyoung Kwak <kjyjykwak@gmail.com>
2024-05-13 22:50:57 +0900

    [Bronze IV] Title: Laptop Sticker, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21591. Laptop Sticker/Laptop Sticker.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int wc, hc, ws, hs;
+    cin >> wc >> hc >> ws >> hs;
+    cout << (((wc - ws) >= 2 && (hc - hs) >= 2) ? 1 : 0) << "\n";
+    
+    return 0;
+}
